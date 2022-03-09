e385da01dddef630c89e7786fd21f89c69b00aba
GolosovRV <golosov437@gmail.com>
2022-03-09 22:57:01 +0300

    Delete file

--- NewMain.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-		String name = "Hello, Roman!";
-        //System.out.println(name);
-		System.out.println("Hello, Romario!");
-
-
-    }
-}
